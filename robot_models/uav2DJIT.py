maybe finally working: fixed alpha3 error in A,b computation, added nomimal controller, tuned gains of nominal controller and of desired state in lyapunov function

robot_models/uav2DJIT.py
@@ -44,7 +44,9 @@ def update_uav_state_jit( state, control, dt ):
     f = uav_f_torch_jit(state)
     g = uav_g_torch_jit(state)
     next_state = state + ( f + g @ control ) * dt
-    print(f"state:{state.T}, next_state:{next_state.T}, f:{f.T}")
+    next_state_copy = next_state.clone()
+    next_state[2,0] = torch.atan2(torch.sin(next_state_copy[2,0]), torch.cos(next_state_copy[2,0]))
+    # print(f"state:{state.T}, next_state:{next_state.T}, f:{f.T}")
     return next_state
 traced_update_si_state_jit = update_uav_state_jit#torch.jit.trace( update_uav_state_jit, ( torch.ones(7,1, dtype=torch.float), torch.ones(2,1, dtype=torch.float), torch.tensor(0.5, dtype=torch.float) ) )
 
@@ -119,12 +121,25 @@ def uav_SI2D_barrier_jit(X, targetX, alpha1, alpha2):#, min_D = torch.tensor(0.3
     dh3_dx = dh_ddot_dx + alpha1 * dh_dot_dx + alpha2 * dh2_dx
     
     return h3, dh3_dx
+
+def uav2D_nominal_input_jit(X,target):
+        phi = X[2,0]
+        ui =  X[3,0]
+        los = X[0:2] - target[0:2]
+        los = los / torch.norm(los)
+        los_angle = torch.atan2( X[1,0]-target[1,0], X[0,0]-target[0,0] )
+        error_angle = los_angle - phi
+        error_angle = torch.atan2( torch.sin(error_angle), torch.cos(error_angle) )
+        tau_r = -5.0 * ( error_angle )
+        tau_u = 5.0 * ( 0.5 - ui ) * torch.sign( -los[0,0]*torch.cos(phi) -los[1,0]*torch.sin(phi) )
+        return torch.cat( (tau_u.reshape(-1,1), tau_r.reshape(-1,1)), dim=0 )
+traced_uav2D_nominal_input_jit = torch.jit.trace( uav2D_nominal_input_jit, ( torch.ones(7,1, dtype=torch.float), torch.zeros(2,1, dtype=torch.float) ) ) 
             
 def uav2D_lyapunov_jit(X, G):
         
-        c1 = 5.0
-        c2 = 5.0
-        c3 = 1.0
+        c1 = 0.5#1.0 # 5.0
+        c2 = 0.5#1.0 #5.0
+        c3 = 0.5#1.0
         
         xi = X[0,0]
         yi = X[1,0]
@@ -173,6 +188,8 @@ def uav2D_lyapunov_jit(X, G):
         
         V = torch.linalg.norm(X-Xdi)**2     
         
+        error_phi = torch.atan2( torch.sin(phi-theta_g), torch.cos(phi-theta_g) )
+        
         dV_dx = torch.cat( (  (2*(X[0,0]-Xdi[0,0]) + 2*(phi-theta_g)*( -dtheta_g_dx[0,0] ) + 2*( ui-c1*dist*torch.cos(theta_g-phi) )*(c1*torch.cos(theta_g-phi)/2/dist*2*(G[0,0]-xi)) +  2*( ui-c1*dist*torch.cos(theta_g-phi) )*( c1*dist*torch.sin(theta_g-phi)*dtheta_g_dx[0,0] )  + 2*( vi-c1*dist*torch.sin(theta_g-phi) )*(c1*torch.sin(theta_g-phi)/2/dist*2*(G[0,0]-xi)) + 2*( vi-c1*dist*torch.sin(theta_g-phi) )*( -c1*dist*torch.cos(theta_g-phi)*dtheta_g_dx[0,0] ) + 2*(ri-c2*(theta_g-phi))*(-c2*dtheta_g_dx[0,0]) + 2*(tau_ui-tau_uid)*(-d_tau_uid_dx[0,0])).reshape(-1,1) , 
                              (2*(X[1,0]-Xdi[1,0]) + 2*(phi-theta_g)*( -dtheta_g_dx[0,1] ) + 2*( ui-c1*dist*torch.cos(theta_g-phi) )*(c1*torch.cos(theta_g-phi)/2/dist*2*(G[1,0]-yi)) +  2*( ui-c1*dist*torch.cos(theta_g-phi) )*( c1*dist*torch.sin(theta_g-phi)*dtheta_g_dx[0,1] )  + 2*( vi-c1*dist*torch.sin(theta_g-phi) )*(c1*torch.sin(theta_g-phi)/2/dist*2*(G[1,0]-yi)) + 2*( vi-c1*dist*torch.sin(theta_g-phi) )*( -c1*dist*torch.cos(theta_g-phi)*dtheta_g_dx[0,1] ) + 2*(ri-c2*(theta_g-phi))*(-c2*dtheta_g_dx[0,1]) + 2*(tau_ui-tau_uid)*(-d_tau_uid_dx[0,1])).reshape(-1,1),
                              (2*( phi-theta_g ) + 2*( ui-c1*dist*torch.cos(theta_g-phi) )*( -c1*dist*(torch.sin(theta_g-phi)) ) + 2*( vi-c1*dist*torch.sin(theta_g-phi) )*( -c1*dist*(-torch.cos(theta_g-phi)) ) + 2*( ri-c2*(theta_g-phi) )*( c2 ) + 2*(tau_ui-tau_uid)*(-d_tau_uid_dx[0,2])).reshape(-1,1),
@@ -191,7 +208,6 @@ def uav2D_lyapunov_jit(X, G):
         
         return V, dV_dx
 
-
     
 # @torch.jit.script
 def uav_compute_reward_jit(X,targetX, control):
@@ -207,10 +223,10 @@ def uav2D_qp_constraints_jit(state, goal, obs1, obs2, param0, param1, param2, pa
     f = uav_f_torch_jit(state)
     g = uav_g_torch_jit(state)
     A0 = -dV_dx @ g; b0 = -dV_dx @ f - param0 * V
-    A1 = dh1_dx @ g; b1 = dh1_dx @ f + param1 * h1
-    A2 = dh2_dx @ g; b2 = dh2_dx @ f + param2 * h2        
-    A = torch.cat( (A0, A1, A2), dim=0 )
-    b = torch.cat( (b0, b1, b2), dim=0 )
+    A1 = dh1_dx @ g; b1 = dh1_dx @ f + param3 * h1
+    A2 = dh2_dx @ g; b2 = dh2_dx @ f + param3 * h2        
+    A = torch.cat( (A0, A1, 0*A2), dim=0 )
+    b = torch.cat( (b0, b1, 0*b2), dim=0 )
     # A = torch.cat( (A0, A1), dim=0 )
     # b = torch.cat( (b0, b1), dim=0 )
     return A, b

robot_models/uav_2d_testing.py
@@ -186,8 +186,8 @@ class UAV_2d:
         
     def lyapunov(self, G):
         
-        c1 = 5.0
-        c2 = 5.0
+        c1 = 1.0
+        c2 = 1.0
         c3 = 1.0
         
         V_w = 0.0#0.1
@@ -222,6 +222,8 @@ class UAV_2d:
         
         # tau_d = 
         
+        error_phi = np.arctan2( np.sin(phi-theta_g), np.cos(phi-theta_g) ) 
+        
         u_di = c1*dist*np.cos(theta_g-phi)
         
         fu = m22*self.X[4,0]*self.X[5,0]+X_u*self.X[3,0]+X_u_u*np.abs(self.X[3,0])*self.X[3,0]
@@ -229,17 +231,19 @@ class UAV_2d:
         tau_uid = -fu + c3*m11*( u_di - ui )
         d_tau_uid_dx = -dfu_dx +  c3*m11*np.array([[ -c1*dist*np.sin(theta_g-phi)*dtheta_g_dx[0,0]+c1*np.cos(theta_g-phi)*(-1.0/dist*(G[0,0]-xi)), -c1*dist*np.sin(theta_g-phi)*dtheta_g_dx[0,1]+c1*np.cos(theta_g-phi)*(-1.0/dist*(G[1,0]-yi)), -c1*dist*np.sin(theta_g-phi), -1, 0, 0, 0   ]])
                        
-        Xdi = np.array( [ G[0,0], G[1,0], theta_g, c1*np.linalg.norm( G[0:2]-self.X[0:2] ) * np.cos(theta_g-self.X[2,0]), c1 * np.linalg.norm( G[0:2]-self.X[0:2] ) * np.sin(theta_g-self.X[2,0]), c2 * (theta_g-self.X[2,0]), 0 ] ).reshape(-1,1) #c3 * ( c1*dist * np.cos(G[2,0]-self.X[2,0]) - self.X[3,0] ) ] ).reshape(-1,1)
+        Xdi = np.array( [ G[0,0], G[1,0], theta_g, c1*np.linalg.norm( G[0:2]-self.X[0:2] ) * np.cos(theta_g-phi), c1 * np.linalg.norm( G[0:2]-self.X[0:2] ) * np.sin(theta_g-phi), c2 * (-error_phi), 0 ] ).reshape(-1,1) #c3 * ( c1*dist * np.cos(G[2,0]-self.X[2,0]) - self.X[3,0] ) ] ).reshape(-1,1)
         # Xdi = np.array( [ G[0,0], G[1,0], G[2,0], c1*np.linalg.norm( G[0:2]-self.X[0:2] ) * np.cos(G[2,0]-self.X[2,0]), c1 * np.linalg.norm( G[0:2]-self.X[0:2] ) * np.sin(G[2,0]-self.X[2,0]), c2 * (G[2,0]-self.X[2,0]), 0 ] ).reshape(-1,1) #c3 * ( c1*dist * np.cos(G[2,0]-self.X[2,0]) - self.X[3,0] ) ] ).reshape(-1,1)
         
         V = np.linalg.norm(self.X-Xdi)**2     
         
-        dV_dx = np.array([[  2*(self.X[0,0]-Xdi[0,0]) + 2*(phi-theta_g)*( -dtheta_g_dx[0,0] ) + 2*( ui-c1*dist*np.cos(theta_g-phi) )*(c1*np.cos(theta_g-phi)/2/dist*2*(G[0,0]-xi)) +  2*( ui-c1*dist*np.cos(theta_g-phi) )*( c1*dist*np.sin(theta_g-phi)*dtheta_g_dx[0,0] )  + 2*( vi-c1*dist*np.sin(theta_g-phi) )*(c1*np.sin(theta_g-phi)/2/dist*2*(G[0,0]-xi)) + 2*( vi-c1*dist*np.sin(theta_g-phi) )*( -c1*dist*np.cos(theta_g-phi)*dtheta_g_dx[0,0] ) + 2*(ri-c2*(theta_g-phi))*(-c2*dtheta_g_dx[0,0]) + 2*(tau_ui-tau_uid)*(-d_tau_uid_dx[0,0]) , 
-                             2*(self.X[1,0]-Xdi[1,0]) + 2*(phi-theta_g)*( -dtheta_g_dx[0,1] ) + 2*( ui-c1*dist*np.cos(theta_g-phi) )*(c1*np.cos(theta_g-phi)/2/dist*2*(G[1,0]-yi)) +  2*( ui-c1*dist*np.cos(theta_g-phi) )*( c1*dist*np.sin(theta_g-phi)*dtheta_g_dx[0,1] )  + 2*( vi-c1*dist*np.sin(theta_g-phi) )*(c1*np.sin(theta_g-phi)/2/dist*2*(G[1,0]-yi)) + 2*( vi-c1*dist*np.sin(theta_g-phi) )*( -c1*dist*np.cos(theta_g-phi)*dtheta_g_dx[0,1] ) + 2*(ri-c2*(theta_g-phi))*(-c2*dtheta_g_dx[0,1]) + 2*(tau_ui-tau_uid)*(-d_tau_uid_dx[0,1]),
-                             2*( phi-theta_g ) + 2*( ui-c1*dist*np.cos(theta_g-phi) )*( -c1*dist*(np.sin(theta_g-phi)) ) + 2*( vi-c1*dist*np.sin(theta_g-phi) )*( -c1*dist*(-np.cos(theta_g-phi)) ) + 2*( ri-c2*(theta_g-phi) )*( c2 ) + 2*(tau_ui-tau_uid)*(-d_tau_uid_dx[0,2]),
+        
+        
+        dV_dx = np.array([[  2*(self.X[0,0]-Xdi[0,0]) + 2*(error_phi)*( -dtheta_g_dx[0,0] ) + 2*( ui-c1*dist*np.cos(theta_g-phi) )*(c1*np.cos(theta_g-phi)/2/dist*2*(G[0,0]-xi)) +  2*( ui-c1*dist*np.cos(theta_g-phi) )*( c1*dist*np.sin(theta_g-phi)*dtheta_g_dx[0,0] )  + 2*( vi-c1*dist*np.sin(theta_g-phi) )*(c1*np.sin(theta_g-phi)/2/dist*2*(G[0,0]-xi)) + 2*( vi-c1*dist*np.sin(theta_g-phi) )*( -c1*dist*np.cos(theta_g-phi)*dtheta_g_dx[0,0] ) + 2*(ri-c2*(-error_phi))*(-c2*dtheta_g_dx[0,0]) + 2*(tau_ui-tau_uid)*(-d_tau_uid_dx[0,0]) , 
+                             2*(self.X[1,0]-Xdi[1,0]) + 2*(error_phi)*( -dtheta_g_dx[0,1] ) + 2*( ui-c1*dist*np.cos(theta_g-phi) )*(c1*np.cos(theta_g-phi)/2/dist*2*(G[1,0]-yi)) +  2*( ui-c1*dist*np.cos(theta_g-phi) )*( c1*dist*np.sin(theta_g-phi)*dtheta_g_dx[0,1] )  + 2*( vi-c1*dist*np.sin(theta_g-phi) )*(c1*np.sin(theta_g-phi)/2/dist*2*(G[1,0]-yi)) + 2*( vi-c1*dist*np.sin(theta_g-phi) )*( -c1*dist*np.cos(theta_g-phi)*dtheta_g_dx[0,1] ) + 2*(ri-c2*(-error_phi))*(-c2*dtheta_g_dx[0,1]) + 2*(tau_ui-tau_uid)*(-d_tau_uid_dx[0,1]),
+                             2*( error_phi ) + 2*( ui-c1*dist*np.cos(theta_g-phi) )*( -c1*dist*(np.sin(theta_g-phi)) ) + 2*( vi-c1*dist*np.sin(theta_g-phi) )*( -c1*dist*(-np.cos(theta_g-phi)) ) + 2*( ri-c2*(theta_g-phi) )*( c2 ) + 2*(tau_ui-tau_uid)*(-d_tau_uid_dx[0,2]),
                              2*( ui-c1*dist*np.cos(theta_g-phi) ) + 2*(tau_ui-tau_uid)*(-d_tau_uid_dx[0,3]),
                              2*( vi-c1*dist*np.sin(theta_g-phi) ) + 2*(tau_ui-tau_uid)*(-d_tau_uid_dx[0,4]),
-                             2*( ri-c2*(theta_g-phi) ) + 2*(tau_ui-tau_uid)*(-d_tau_uid_dx[0,5]),
+                             2*( ri-c2*(-error_phi) ) + 2*(tau_ui-tau_uid)*(-d_tau_uid_dx[0,5]),
                              2*(tau_ui-tau_uid) + 2*(tau_ui-tau_uid)*(-d_tau_uid_dx[0,6])  ]])    
         
         # dV_dx = np.array([[  2*(self.X[0,0]-Xdi[0,0]) + 2*( ui-c1*dist*np.cos(G[2,0]-phi) )/2/dist*2*(self.X[0,0]-xi) + 2*( vi-c1*dist*np.sin(G[2,0]-phi) )/2/dist*2*(self.X[0,0]-xi)  , 
@@ -276,7 +280,7 @@ class UAV_2d:
         if error_angle < -np.pi:
             error_angle = error_angle + 2*np.pi
         tau_r = -5.0 * ( error_angle )
-        tau_u = 10.0 * ( 0.5 - ui ) * np.sign( -los[0,0]*np.cos(phi) -los[1,0]*np.sin(phi) )
+        tau_u = 5.0 * ( 0.5 - ui ) * np.sign( -los[0,0]*np.cos(phi) -los[1,0]*np.sin(phi) )
         return np.array([ tau_u, tau_r ]).reshape(-1,1)
         
         
@@ -371,11 +375,13 @@ if 1:
     dt = 0.01
     tf = 20
     num_steps = int(tf/dt)
-    alpha3 = 0.8
-    robot = UAV_2d( np.array([0,0,0,0.1,0.1,0,0.1]).reshape(-1,1), dt, ax, alpha1 = 2.0, alpha2 = 2.0 )
+    alpha3 = 10.0
+    robot = UAV_2d( np.array([-0.5,-2.5,0,0.1,0.1,0,0.1]).reshape(-1,1), dt, ax, alpha1 = 2.0, alpha2 = 3.0 )
+    # 1,3,5 ok.. from top
+    # 2,3,10 better
     obsX = [0.5, 0.5]
     obs2X = [1.5, 1.9]
-    targetX = np.array([0.3, 0.3]).reshape(-1,1)
+    targetX = np.array([0.9, 0.9]).reshape(-1,1)
     d_min = 0.3
     obs1 = circle2D(obsX[0], obsX[1], d_min, ax, 0)
     obs2 = circle2D(obs2X[0], obs2X[1], d_min, ax, 0)
@@ -391,8 +397,8 @@ if 1:
     objective = cp.Minimize( cp.sum_squares( u - u_ref ) + 10 * cp.sum_squares( delta[0,0] ) + 100000 * cp.sum_squares(delta[1:,:]) ) 
     # factor_matrix = np.zeros((num_constraints,1)); factor_matrix[0,0] = 1
     const = [A1 @ u + b1 + delta >= 0]
-    const += [ cp.abs( u[0,0] ) <= 50.0 ] #5
-    const += [ cp.abs( u[1,0] ) <= 50.0 ] # 2.0
+    const += [ cp.abs( u[0,0] ) <= 10.0 ] #5
+    const += [ cp.abs( u[1,0] ) <= 10.0 ] # 2.0
     cbf_controller = cp.Problem( objective, const )
     
     for i in range(num_steps):
@@ -403,14 +409,14 @@ if 1:
         u_ref.value = robot.nominal_input( targetX )
         # print(f"V:{V}, dV_dx:{dV_dx}")
         A1.value[0,:] = 1.0*(-dV_dx @ robot.g())
-        A1.value[1,:] = 0.0*(dh3_dx @ robot.g())
+        A1.value[1,:] = 1.0*(dh3_dx @ robot.g())
         b1.value[0,:] = 1.0*(-dV_dx @ robot.f() - 1.0 * V )
-        b1.value[1,:] = 0.0*(dh3_dx @ robot.f() + alpha3 * h3)
+        b1.value[1,:] = 1.0*(dh3_dx @ robot.f() + alpha3 * h3)
         cbf_controller.solve(solver=cp.GUROBI, reoptimize=True)
         
         if cbf_controller.status!='optimal':
             print("ERROR in QP")
-                  
+        print(f"A:{A1.value}, b:{b1.value}, ref:{u_ref.value}")
         print(f"control input: {u.value.T}")
         robot.step(u.value)
         # print(f"ui:{robot.X[3,0]}, vi:{robot.X[4,0]}")

uav_2d/mpc_cbf_sqp.py
@@ -17,21 +17,28 @@ tf = 40
 # N = int( tf/dt_inner )
 # N = 100#100
 # tf =  int( N * dt_inner ) #20
-outer_loop = 2
+outer_loop = 2#0000000
 num_gd_iterations = 1
-dt_outer = 0.05
+dt_outer = 0.01
 H = 30#100
 lr_alpha = 0.01#0.05
 plot_x_lim = (-1.0,3.5)  
-plot_y_lim = (-0.8,3) 
+# plot_y_lim = (-0.8,3) 
+plot_y_lim = (-2.6,3)
 
 # starting point
 # X_init = np.array([-0.5,-0.5,np.pi/2])
-X_init = np.array([-0.5,-0.5, 0, 0.1, 0.1, 0, 0.1 ])
 d_obs = 0.3
-goalX = np.array([2.5,2.0])
+X_init = np.array([0.3,-1.0, np.pi/4, 0.1, 0.1, 0, 0.1 ]) #-0.3,-0.5
+goalX = np.array([2.5,2.0]).reshape(-1,1)
 obs1X = [0.7, 0.7]
 obs2X = [1.5, 1.9]
+
+# X_init = np.array([-0.5,-2.5, 0, 0.1, 0.1, 0, 0.1 ])
+# goalX = np.array([0.9, 0.9]).reshape(-1,1)
+# goalX = np.array([1.5, 2.4]).reshape(-1,1)
+# obs1X = [0.5, 0.5]
+# obs2X = [1.5, 1.9]
 
 # input bounds
 u1_max = 10.0
@@ -79,16 +86,19 @@ def compute_reward(robot, obs1, obs2, params, dt_outer):
     improve_constraints = []  
     global H
     for i in range(H):
-        print(f"FROM loop states:{states[i].T}")
+        # print(f"FROM loop states:{states[i].T}")
         # make  control matrices
-        control_ref = torch.tensor([0,0], dtype=torch.float).reshape(-1,1)
+        # control_ref = torch.tensor([0,0], dtype=torch.float).reshape(-1,1)
+        control_ref = uav2D_nominal_input_jit( states[i], robot.goal_torch )
         A, b = traced_uav2D_qp_constraints_jit( states[i], robot.goal_torch, obs1.X_torch, obs2.X_torch, params[0], params[1], params[2], params[3] )
-        
+        # print(f"A:{A}, b:{b}, u_ref:{control_ref}")
         control, deltas = cbf_controller_layer( control_ref, A, b ) #if ( (torch.abs(control[0,0]) > 11) or (torch.abs(control[1,0])>11) ): # print("wrong input: issue with solver")
         
-        if ( np.abs(control[0,0].detach().numpy())>u1_max ) or ( np.abs(control[1,0].detach().numpy())>u2_max ):
+        if ( np.abs(control[0,0].detach().numpy())>1.1*u1_max ) or ( np.abs(control[1,0].detach().numpy())>1.1*u2_max ):
             print("solved Inaccurate")
-            
+            # plt.ioff()
+            # plt.show()
+            # plt.ion()
             A, b = uav2D_qp_constraints_jit( states[i], robot.goal_torch, obs1.X_torch, obs2.X_torch, params[0], params[1], params[2], params[3] )
             
             return reward, improve_constraints, maintain_constraints, True
@@ -114,7 +124,7 @@ def compute_reward(robot, obs1, obs2, params, dt_outer):
                    
         # Get next state
         next_state = update_uav_state_jit( states[i], control, dt_outer )
-        print(f"FROM loop: next_state:{next_state.T}, control:{control.T}")
+        # print(f"FROM loop: next_state:{next_state.T}, control:{control.T}")
                 
         # Save next state and compute reward
         states.append( next_state )
@@ -222,11 +232,13 @@ def simulate_scenario( movie_name = 'test.mp4', adapt = True, enforce_input_cons
                 robot.X_torch = torch.tensor(robot.X, dtype=torch.float)
             
                 # get controller
-                control_ref = torch.tensor([0,0], dtype=torch.float).reshape(-1,1)
+                # control_ref = torch.tensor([0,0], dtype=torch.float).reshape(-1,1)
+                control_ref = traced_uav2D_nominal_input_jit( robot.X_torch, robot.goal_torch )
                 A, b = traced_uav2D_qp_constraints_jit( robot.X_torch, robot.goal_torch, obs1.X_torch, obs2.X_torch, torch.tensor(params[0], dtype=torch.float), torch.tensor(params[1], dtype=torch.float), torch.tensor(params[2], dtype=torch.float), torch.tensor(params[3], dtype=torch.float) )
-                control, deltas = cbf_controller_layer( control_ref, A, b )
-                print(f"t:{t}, actual: {control.T}") 
-                # print(f"control: {control.T}")
+                # print(f"A:{A}, b:{b}, ref:{control_ref}")
+                control, deltas = cbf_controller_layer( control_ref, A, b, solver_args=solver_args )
+                # print(f"t:{t}, actual: {control.T}") 
+                print(f"control: {control.T}")
                 robot.step( control.detach().numpy() )
                 robot.render_plot()
                 
@@ -237,7 +249,8 @@ def simulate_scenario( movie_name = 'test.mp4', adapt = True, enforce_input_cons
                 # Nominal robot
                 if not nominal_failed:
                     robot.X_nominal_torch = torch.tensor(robot.X_nominal, dtype=torch.float)
-                    control_ref = torch.tensor([0,0], dtype=torch.float).reshape(-1,1)
+                    # control_ref = torch.tensor([0,0], dtype=torch.float).reshape(-1,1)
+                    control_ref = uav2D_nominal_input_jit( robot.X_nominal_torch, robot.goal_torch )
                     A, b = traced_uav2D_qp_constraints_jit( robot.X_nominal_torch, robot.goal_torch, obs1.X_torch, obs2.X_torch, torch.tensor(params_copy[0], dtype=torch.float), torch.tensor(params_copy[1], dtype=torch.float), torch.tensor(params_copy[2], dtype=torch.float), torch.tensor(params_copy[3], dtype=torch.float) )
                     control, deltas = cbf_controller_layer( control_ref, A, b )  
                     if np.any( deltas[1:].detach().numpy() > 0.1 ):
@@ -299,8 +312,8 @@ def simulate_scenario( movie_name = 'test.mp4', adapt = True, enforce_input_cons
             
             
 # Run simulations
-# fig1, ax1, robot1, rewards1, params1 = simulate_scenario( movie_name = 'uav_2d/figures/cs4_case1_rc.mp4', adapt=True, enforce_input_constraints=True, params = [1.0, 4.0, 3.0, 2.0], plot_x_lim = plot_x_lim, plot_y_lim = plot_y_lim, offline = False, offline_iterations=20 )            
-fig1, ax1, robot1, rewards1, params1 = simulate_scenario( movie_name = 'RateTunableCBFs/uav_2d/figures/cs4_case1_rc.mp4', adapt=True, enforce_input_constraints=True, params = [1.0, 4.0, 3.0, 2.0], plot_x_lim = plot_x_lim, plot_y_lim = plot_y_lim, offline = False, offline_iterations=20 )            
+fig1, ax1, robot1, rewards1, params1 = simulate_scenario( movie_name = 'uav_2d/figures/cs4_case1_rc.mp4', adapt=True, enforce_input_constraints=True, params = [1.0, 2.0, 4.0, 20.0], plot_x_lim = plot_x_lim, plot_y_lim = plot_y_lim, offline = False, offline_iterations=20 )            
+# fig1, ax1, robot1, rewards1, params1 = simulate_scenario( movie_name = 'RateTunableCBFs/uav_2d/figures/cs4_case1_rc.mp4', adapt=True, enforce_input_constraints=True, params = [1.0, 2.0, 3.0, 10.0], plot_x_lim = plot_x_lim, plot_y_lim = plot_y_lim, offline = False, offline_iterations=20 )            
 # fig2, ax2, robot2, rewards2, params2 = simulate_scenario( movie_name = 'uav_2d/figures/cs4_case2_rc.mp4', adapt=True, enforce_input_constraints=True, params = [0.5, 0.5, 0.5], plot_x_lim = plot_x_lim, plot_y_lim = plot_y_lim, offline = False, offline_iterations=20 )            
 # fig3, ax3, robot3, rewards3, params3 = simulate_scenario( movie_name = 'uav_2d/figures/cs4_case1_offline.mp4', adapt=True, enforce_input_constraints=True, params = [1.0, 3.0, 3.0], plot_x_lim = plot_x_lim, plot_y_lim = plot_y_lim, offline = True, offline_iterations=20 )            
 # fig4, ax4, robot4, rewards4, params4 = simulate_scenario( movie_name = 'uav_2d/figures/cs4_case2_offline.mp4', adapt=True, enforce_input_constraints=True, params = [0.5, 0.5, 0.5], plot_x_lim = plot_x_lim, plot_y_lim = plot_y_lim, offline = True, offline_iterations=20 )